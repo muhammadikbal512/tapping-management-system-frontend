49d040ea318853bc72e6c2d8de1707d5405968eb
muhammadikbal512 <muhammadikbal512@gmail.com>
2022-03-15 16:29:39 +0700

    create user model

---
+++ src/app/model/user-model/user.model.ts
@@ -0,0 +1,26 @@
+export class UserModel {
+    id!: number;
+    userId!: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    logInDateDisplay!: Date;
+    joinDate!: Date;
+    profileImageUrl?: string;
+    active: boolean;
+    notLocked: boolean;
+    role: string;
+    authorities: [];
+
+    constructor() {
+        this.firstName = '';
+        this.lastName = '';
+        this.username = '';
+        this.email = '';
+        this.active = false;
+        this.notLocked = false;
+        this.role = '';
+        this.authorities = [];
+      }
+}
