eb90f69f413d10663507c41aa6d54b3e459ea1b6
muhammadikbal512 <muhammadikbal512@gmail.com>
2022-03-18 16:55:35 +0700

    login update

--- src/app/modules/module/login/login.component.ts
+++ src/app/modules/module/login/login.component.ts
@@ -6,47 +6,46 @@ import { Subscription } from 'rxjs';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { UserModel } from 'src/app/model/user-model/user.model';
 import { HeaderType } from 'src/app/enum/header-type';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  
   private subscriptions: Subscription[] = [];
   form!: FormGroup;
-
-
+  public showLoading?: boolean;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private authenticationService: AuthenticationService,
     private fb: FormBuilder
-  ) { }
+  ) {}
 
   ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
   ngOnInit(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
-    })
+    });
 
     if (this.authenticationService.isLoggedIn()) {
       this.router.navigateByUrl('/TMS-Home/dashboard');
     } else {
-      
     }
   }
 
-
   login(user: UserModel): void {
-    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || 'TMS-Home/dashboard';
+    let returnUrl =
+      this.route.snapshot.queryParamMap.get('returnUrl') ||
+      'TMS-Home/dashboard';
+      this.showLoading = true;
     this.subscriptions.push(
       this.authenticationService.login(user).subscribe(
         (response: HttpResponse<UserModel> | any) => {
@@ -56,12 +55,27 @@ export class LoginComponent implements OnInit, OnDestroy {
           }
           this.authenticationService.addUserToLocalCache(response.body);
           this.router.navigateByUrl(returnUrl);
+          this.showLoading = false;
         },
         (errorResponse: HttpErrorResponse) => {
-          alert('Username or password is wrong')
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer);
+              toast.addEventListener('mouseleave', Swal.resumeTimer);
+            },
+          });
+          Toast.fire({
+            icon: 'error',
+            title: 'Username or password is wrong !',
+          });
+          this.showLoading = false;
         }
       )
-    )
-    
+    );
   }
 }
