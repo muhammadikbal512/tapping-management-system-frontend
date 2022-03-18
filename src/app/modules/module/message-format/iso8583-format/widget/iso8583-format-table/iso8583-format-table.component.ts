d9d99866e839f9d4ce7e085cf313fa56b21a9c11
muhammadikbal512 <muhammadikbal512@gmail.com>
2022-03-18 16:58:39 +0700

    message format , format table edit

--- src/app/modules/module/message-format/iso8583-format/widget/iso8583-format-table/iso8583-format-table.component.ts
+++ src/app/modules/module/message-format/iso8583-format/widget/iso8583-format-table/iso8583-format-table.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-iso8583-format-table',
   templateUrl: './iso8583-format-table.component.html',
-  styleUrls: ['./iso8583-format-table.component.css']
+  styleUrls: ['./iso8583-format-table.component.css'],
 })
 export class Iso8583FormatTableComponent implements OnInit {
+  columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+  ];
 
-  constructor() { }
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
